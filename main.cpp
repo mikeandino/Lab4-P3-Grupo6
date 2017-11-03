bd20e90a2c1e21f617890d21fde2123587557031
jefryhdz <jefryhdz@gmail.com>
2017-11-03 17:54:50 -0600

    primer commit

--- main.cpp
+++ main.cpp
@@ -44,10 +44,45 @@ int main(){
 	}//fin de while
 	return 0;
 }
+int** crear(int size){
+	int** mat = new int*[size]:
+	for(int i=0;i<size;i++){
+		mat[i]= new int[size];
+	}
+	return mat;
 
 
+}
+int** llenar(int** matriz, int size){
+	for(int i =0;i<size;i++){
+		for(int j=0;j<size;j++){
+			matriz[i][j]=0;
+		}
+	}
+}void prueba(int** mat,int x, int y,int size){
+	if(mat[x][y]!=0){
+		
+	}else{
+		if(i<size-1){
+			if(mat[x][y]==0 ){
 
-
-
-
+			}
+		}
+	}
+			
+}void print(){
+	initscr();
+	noecho();
+	WINDOW* win = newwin(6,6,5,5);
+	for(int i=1;i<6;i++){
+		for(int j=1;j<6;j++){
+			string temp="1";
+			mvwprintw(win,i,j, temp.c_str());
+			wrefresh(win);
+			getch();
+		}
+	}	
+	endwin();
+}
+}
 
